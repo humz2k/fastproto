01af82674b28c349a601219639617a3cf4a4185f
Humza Qureshi <humzaqureshis@gmail.com>
2026-05-28 14:10:07 -0500

    ci: publish package to pypi on tags

--- fastproto_compiler/__init__.py
+++ fastproto_compiler/__init__.py
@@ -1,6 +1,12 @@
 """fastproto compiler package."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .generator import generate_code
 
-__all__ = ["generate_code"]
-__version__ = "0.1.0"
+try:
+    __version__ = version("fastproto-compiler")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
+__all__ = ["__version__", "generate_code"]
